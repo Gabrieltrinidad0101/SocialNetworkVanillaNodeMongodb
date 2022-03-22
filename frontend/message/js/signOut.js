d65ed1ecc452f2cabc8085ec83bcff61a50dda81
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-03-21 23:21:20 -0400

    fix error in z-index

--- frontend/message/js/signOut.js
+++ frontend/message/js/signOut.js
@@ -1,5 +1,4 @@
 const signOutButton = document.getElementById("signOut");
-
 signOutButton.addEventListener("click",_=>{
     localStorage.setItem("token","");
     window.location = "/frontend/register/register.html"
