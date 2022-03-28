1294396267bfc74f4eacedd8f4c4b7d78ef9a744
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-03-27 21:54:26 -0400

    adding messages

--- backend/src/routers/user/user.js
+++ backend/src/routers/user/user.js
@@ -4,7 +4,7 @@ const User = require("../../model/user")
 const router = Router();
 
 router.get("/get",verifyAccount,async (req,res)=>{
-    const users = await User.find({id: {$ne: req.userId }},{name: 1})
+    const users = await User.find({_id: {$ne: req.userId }},{name: 1})
     res.status(200).json({message: users});
 })
 
